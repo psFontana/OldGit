de00e69853ecd8c43856a1f470e3df92c30bcc75
Paulo Fontana <86742378+AshoZard@users.noreply.github.com>
2021-07-21 16:48:44 -0300

    Update index.js

---
+++ javaScript/index.js
@@ -0,0 +1,49 @@
+class cliente {
+
+    nome;
+    cpf;
+
+}
+
+class contaCorrente {
+
+    agencia;
+    saldo;
+
+    sacar(valor){
+
+        if(this.saldo >= valor) {
+
+            this.saldo -= valor;
+
+        }
+
+    };
+
+    depositar(valor){
+
+        this.saldo += valor;
+
+    }
+
+}
+
+const cliente1 = new cliente();
+cliente1.nome = "Marcus";
+cliente1.cpf = 11748293042;
+
+
+const cliente2 = new cliente();
+cliente2.nome = "Jean";
+cliente2.cpf = 74283492394;
+
+
+//teste
+
+const contaCorrente1 = new contaCorrente();
+contaCorrente1.agencia = 1001;
+contaCorrente1.saldo = 0;
+contaCorrente1.depositar(100);
+console.log(contaCorrente1.saldo);
+contaCorrente1.sacar(40);
+console.log(contaCorrente1.saldo);
