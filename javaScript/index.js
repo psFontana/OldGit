8536936f409ced592db7732f977ea152278ec914
Paulo Fontana <86742378+AshoZard@users.noreply.github.com>
2021-07-28 15:54:00 -0300

    .

--- javaScript/index.js
+++ javaScript/index.js
@@ -38,14 +38,3 @@ cliente1.cpf = 11748293042;
 const cliente2 = new cliente();
 cliente2.nome = "Jean";
 cliente2.cpf = 74283492394;
-
-
-//teste
-
-const contaCorrente1 = new contaCorrente();
-contaCorrente1.agencia = 1001;
-contaCorrente1.saldo = 0;
-contaCorrente1.depositar(100);
-console.log(contaCorrente1.saldo);
-contaCorrente1.sacar(40);
-console.log(contaCorrente1.saldo);
