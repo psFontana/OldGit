37f34bdcedd2b2e52ef4909719a7c1fa4022143b
AshoZard <paulo.fontana@escola.pr.gov.br>
2021-12-08 15:47:11 -0300

    .

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
-import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeNotas from "./components/ListaDeNotas";
-
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
+
+  criarNota(titulo, texto){
+    console.log(`uma nova nota foi criada ` + titulo + " " + texto)
+  }
   render() {
     return (
-      <section>
-        <FormularioCadastro />
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota}/>
         <ListaDeNotas />
       </section>
     );
   }
 }
+
 
 export default App;
