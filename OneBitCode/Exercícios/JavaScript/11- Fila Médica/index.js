0700be84736c40fe86e3294eab9e326c8f50212c
Paulo Fontana <86742378+AshoZard@users.noreply.github.com>
2023-02-09 13:54:51 -0300

    .

--- OneBitCode/Exercícios/JavaScript/11- Fila Médica/index.js
+++ OneBitCode/Exercícios/JavaScript/11- Fila Médica/index.js
@@ -1,29 +1,35 @@
 let lista = [];
-let nome;
-pacientes = lista.length;
-let opcao = 1;
-let saida = "";
-while (opcao != 3) {
+let opcao = "";
+do {
+  let saida = "";
+  for (i = 0; i < lista.length; i++) {
+    saida += i + 1 + ". " + lista[i] + "\n";
+  }
+
   opcao = parseFloat(
     prompt(
       "Olá, a ordem da lista de espera é a seguinte:\n" +
-        lista +
+        saida +
         "\nO que deseja fazer?\n 1) Adicionar Paciente á lista de espera.\n 2) Atender o primeiro Paciente da fila.\n 3) Sair."
     )
   );
 
   switch (opcao) {
     case 1:
-      nome = prompt("Qual o nome do Paciente a ser adicionado na fila?");
-      for (i = 0; i <= pacientes; i++) {
-        lista.push(nome + " ");
-      }
+      let nome = prompt("Qual o nome do Paciente a ser adicionado na fila?");
+      lista.push(nome);
       break;
     case 2:
-      lista.shift();
+      let removido = lista.shift();
+      if (removido === undefined) {
+        alert("Não há pacientes na fila.");
+      }
+      alert("O Paciente " + removido + " foi atendido.");
       break;
     case 3:
       alert("Encerrando sistema...");
       break;
+    default:
+      alert("Opcção inválida");
   }
-}
+} while (opcao != 3);
