60898fca48ea7562b47a9f87f0a328e30beb4655
Paulo Fontana <86742378+AshoZard@users.noreply.github.com>
2023-06-05 00:55:08 -0300

    .

--- teste.c
+++ teste.c
@@ -72,7 +72,7 @@ int main(){
   char palavra[100];
   scanf("%s", palavra);
   int vogais = contaVogal(palavra);
-  int consoantes = strlen(palavra) - vogais - 1;
+  int consoantes = strlen(palavra) - vogais;
   if (vogais > consoantes)
   {
     printf("Ha mais vogais que consoantes.");
