83c5efc580065f0c2142ee0b2d30858b4fccc66e
psFontana <166160927+psFontana@users.noreply.github.com>
2025-01-20 18:29:24 -0300

    .

--- UTFPR/Sistemas Operacionais/Atividade Extra/Final/index.js
+++ UTFPR/Sistemas Operacionais/Atividade Extra/Final/index.js
@@ -1,8 +1,12 @@
 let numero = document.getElementById("numero");
 
-let alterado = document.querySelectorAll(".fifoDiv .container .number")[2];
-
 function fifo() {
+    for (let i = 0; i < 3; i++) {
+        if (document.querySelectorAll(".fifoDiv .container .number")[i].children[0].innerText == numero.value) {
+            return
+        }
+    }
+
     let n3 = document.getElementById("n-3");
     if (n3.classList.contains("n-1")) {
         document.getElementById("n-3").classList.remove("n-1");
@@ -33,66 +37,80 @@ function fifo() {
 }
 
 function lru() {
-    let verificacao = false;
+    let verificacao = -1;
     for (let i = 0; i < 3; i++) {
         if (document.querySelectorAll(".lruDiv .container .number")[i].children[0].innerText === numero.value) {
             verificacao = i;
         }
     }
-    console.log(verificacao)
-    if (verificacao === false || document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.contains("n-3")) {
-        let n3 = document.getElementById("n-6");
-        if (n3.classList.contains("n-1")) {
-            document.getElementById("n-6").classList.remove("n-1");
-            document.getElementById("n-6").classList.add("n-2");
-            document.getElementById("n-5").classList.remove("n-3");
-            document.getElementById("n-5").classList.add("n-1");
-            document.getElementById("n-4").classList.remove("n-2");
-            document.getElementById("n-4").classList.add("n-3");
-            alterado = document.querySelectorAll(".lruDiv .container .number")[1]
-        } else if (n3.classList.contains("n-2")) {
-            document.getElementById("n-6").classList.remove("n-2");
-            document.getElementById("n-6").classList.add("n-3");
-            document.getElementById("n-5").classList.remove("n-1");
-            document.getElementById("n-5").classList.add("n-2");
-            document.getElementById("n-4").classList.remove("n-3");
-            document.getElementById("n-4").classList.add("n-1");
-            alterado = document.querySelectorAll(".lruDiv .container .number")[0]
-        } else {
-            document.getElementById("n-6").classList.remove("n-3");
-            document.getElementById("n-6").classList.add("n-1");
-            document.getElementById("n-5").classList.remove("n-2");
-            document.getElementById("n-5").classList.add("n-3");
-            document.getElementById("n-4").classList.remove("n-1");
-            document.getElementById("n-4").classList.add("n-2");
-            alterado = document.querySelectorAll(".lruDiv .container .number")[2]
+    if (verificacao == -1 || document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.contains("n-3")) {
+        if (document.querySelectorAll(".lruDiv .container .number")[0].classList.contains("n-3")) {
+            document.querySelectorAll(".lruDiv .container .number")[0].children[0].innerText = numero.value
+            document.querySelectorAll(".lruDiv .container .number")[0].classList.remove("n-3")
+            document.querySelectorAll(".lruDiv .container .number")[0].classList.add("n-1")
+            if (document.querySelectorAll(".lruDiv .container .number")[1].classList.contains("n-2")) {
+                document.querySelectorAll(".lruDiv .container .number")[1].classList.remove("n-2")
+                document.querySelectorAll(".lruDiv .container .number")[1].classList.add("n-3")
+                document.querySelectorAll(".lruDiv .container .number")[2].classList.remove("n-1")
+                document.querySelectorAll(".lruDiv .container .number")[2].classList.add("n-2")
+            } else {
+                document.querySelectorAll(".lruDiv .container .number")[1].classList.remove("n-1")
+                document.querySelectorAll(".lruDiv .container .number")[1].classList.add("n-2")
+                document.querySelectorAll(".lruDiv .container .number")[2].classList.remove("n-2")
+                document.querySelectorAll(".lruDiv .container .number")[2].classList.add("n-3")
+            }
+        } else if (document.querySelectorAll(".lruDiv .container .number")[1].classList.contains("n-3")) {
+            document.querySelectorAll(".lruDiv .container .number")[1].children[0].innerText = numero.value
+            document.querySelectorAll(".lruDiv .container .number")[1].classList.remove("n-3")
+            document.querySelectorAll(".lruDiv .container .number")[1].classList.add("n-1")
+            if (document.querySelectorAll(".lruDiv .container .number")[0].classList.contains("n-2")) {
+                document.querySelectorAll(".lruDiv .container .number")[0].classList.remove("n-2")
+                document.querySelectorAll(".lruDiv .container .number")[0].classList.add("n-3")
+                document.querySelectorAll(".lruDiv .container .number")[2].classList.remove("n-1")
+                document.querySelectorAll(".lruDiv .container .number")[2].classList.add("n-2")
+            } else {
+                document.querySelectorAll(".lruDiv .container .number")[0].classList.remove("n-1")
+                document.querySelectorAll(".lruDiv .container .number")[0].classList.add("n-2")
+                document.querySelectorAll(".lruDiv .container .number")[2].classList.remove("n-2")
+                document.querySelectorAll(".lruDiv .container .number")[2].classList.add("n-3")
+            }
+        } else if(document.querySelectorAll(".lruDiv .container .number")[2].classList.contains("n-3")){
+            document.querySelectorAll(".lruDiv .container .number")[2].children[0].innerText = numero.value
+            document.querySelectorAll(".lruDiv .container .number")[2].classList.remove("n-3")
+            document.querySelectorAll(".lruDiv .container .number")[2].classList.add("n-1")
+            if (document.querySelectorAll(".lruDiv .container .number")[0].classList.contains("n-1")) {
+                document.querySelectorAll(".lruDiv .container .number")[0].classList.remove("n-1")
+                document.querySelectorAll(".lruDiv .container .number")[0].classList.add("n-2")
+                document.querySelectorAll(".lruDiv .container .number")[1].classList.remove("n-2")
+                document.querySelectorAll(".lruDiv .container .number")[1].classList.add("n-3")
+            }else{
+                document.querySelectorAll(".lruDiv .container .number")[1].classList.remove("n-1")
+                document.querySelectorAll(".lruDiv .container .number")[1].classList.add("n-2")
+                document.querySelectorAll(".lruDiv .container .number")[2].classList.remove("n-2")
+                document.querySelectorAll(".lruDiv .container .number")[2].classList.add("n-3")
+            }
         }
-        alterado.children[0].innerText = numero.value
-    }else{
-        if(document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.contains("n-1")){
+    } else {
+        if (document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.contains("n-1")) {
             return
-        }else if(document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.contains("n-2")){
-            if(document.querySelectorAll(".lruDiv .container .number")[0].classList.contains("n-1")){
+        } else if (document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.contains("n-2")) {
+            if (document.querySelectorAll(".lruDiv .container .number")[0].classList.contains("n-1")) {
                 document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.remove("n-2")
                 document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.add("n-1")
                 document.querySelectorAll(".lruDiv .container .number")[0].classList.remove("n-1")
                 document.querySelectorAll(".lruDiv .container .number")[0].classList.add("n-2")
                 return
-            }else if(document.querySelectorAll(".lruDiv .container .number")[1].classList.contains("n-1")){
+            } else if (document.querySelectorAll(".lruDiv .container .number")[1].classList.contains("n-1")) {
                 document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.remove("n-2")
                 document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.add("n-1")
                 document.querySelectorAll(".lruDiv .container .number")[1].classList.remove("n-1")
                 document.querySelectorAll(".lruDiv .container .number")[1].classList.add("n-2")
                 return
-            }else{
+            } else {
                 document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.remove("n-2")
                 document.querySelectorAll(".lruDiv .container .number")[verificacao].classList.add("n-1")
                 document.querySelectorAll(".lruDiv .container .number")[2].classList.remove("n-1")
                 document.querySelectorAll(".lruDiv .container .number")[2].classList.add("n-2")
-                if(verificacao == 0){
-                    document.querySelectorAll(".lruDiv .container .number")[1].classList.add("n-2")
-                    document.querySelectorAll(".lruDiv .container .number")[1].classList.add("n-2")
-                }
             }
         }
     }
