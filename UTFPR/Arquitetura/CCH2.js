0c1061f513acee2fe3ec6ceb1a5369882f3f6026
psFontana <166160927+psFontana@users.noreply.github.com>
2025-02-06 14:30:40 -0300

    .

--- UTFPR/Arquitetura/CCH2.js
+++ UTFPR/Arquitetura/CCH2.js
@@ -10,21 +10,18 @@ class Tag {
   }
 }
 
-class TagProxy {
+class TagProxy extends Tag {
   constructor(tag) {
+    super(tag.name, tag.id, ...tag.children);
     this.tag = tag;
   }
 
   render() {
     try {
-      console.log(
-        `Renderizando a tag <${this.tag.name}> com id: ${this.tag.id}`
-      );
+      console.log(`Renderizando a tag <${this.tag.name}> com id: ${this.tag.id}`);
       return this.tag.render();
     } catch (error) {
-      console.error(
-        `Erro ao renderizar a tag <${this.tag.name}>: ${error.message}`
-      );
+      console.error(`Erro ao renderizar a tag <${this.tag.name}>: ${error.message}`);
       return "";
     }
   }
@@ -34,12 +31,11 @@ class Button extends Tag {
   constructor(id, color, ...childs) {
     super("button", id, ...childs);
     this.color = color;
-    this.proxy = new TagProxy(this);
   }
 
   render() {
     return `<button id="${this.id}" style="color:${this.color}">${this.children
-      .map((child) => child.render())
+      .map((child) => new TagProxy(child).render())
       .join("")}</button>`;
   }
 }
@@ -47,12 +43,11 @@ class Button extends Tag {
 class Div extends Tag {
   constructor(id, ...childs) {
     super("div", id, ...childs);
-    this.proxy = new TagProxy(this);
   }
 
   render() {
     return `<div id="${this.id}">${this.children
-      .map((child) => child.render())
+      .map((child) => new TagProxy(child).render())
       .join("")}</div>`;
   }
 }
@@ -61,12 +56,11 @@ class H1 extends Tag {
   constructor(id, innerText, ...childs) {
     super("h1", id, ...childs);
     this.innerText = innerText;
-    this.proxy = new TagProxy(this);
   }
 
   render() {
     return `<h1 id="${this.id}">${this.innerText} ${this.children
-      .map((child) => child.render())
+      .map((child) => new TagProxy(child).render())
       .join("")}</h1>`;
   }
 }
@@ -75,22 +69,20 @@ class Ancora extends Tag {
   constructor(id, src, ...childs) {
     super("a", id, ...childs);
     this.src = src;
-    this.proxy = new TagProxy(this);
   }
 
   render() {
     return `<a id="${this.id}" href="${this.src}">${this.children
-      .map((child) => child.render())
+      .map((child) => new TagProxy(child).render())
       .join("")}</a>`;
   }
 }
 
 class Img extends Tag {
-  constructor(id, src, alt, ...childs) {
-    super("img", id, ...childs);
+  constructor(id, src, alt) {
+    super("img", id);
     this.src = src;
     this.alt = alt || id;
-    this.proxy = new TagProxy(this);
   }
 
   render() {
@@ -102,20 +94,20 @@ class P extends Tag {
   constructor(id, innerText, ...childs) {
     super("p", id, ...childs);
     this.innerText = innerText;
-    this.proxy = new TagProxy(this);
   }
 
   render() {
     return `<p id="${this.id}">${this.innerText} ${this.children
-      .map((child) => child.render())
+      .map((child) => new TagProxy(child).render())
       .join("")}</p>`;
   }
 }
 
 let parag = new P("firstP", "Primeiro Parágrafo");
 let image = new Img("firstImg", "./minhaImagem", "imagemQualquer");
-let ancora = new Ancora("firstA", "https://www.google.com");
-let title = new H1("firstH1", "Título", parag);
+let ancora = new Ancora("firstA", "https://www.google.com", parag);
+let title = new H1("firstH1", "Título", ancora);
 let main = new Div("main", title, image);
 
-console.log(main.proxy.render());
+let proxyMain = new TagProxy(main);
+console.log(proxyMain.render());
